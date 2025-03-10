260429ea9a6a26c72850c3e7e7285410e3eb5fcc
AgentLawliet <khandurishekhar246@gmail.com>
2025-03-10 18:00:04 +0000

    Your commit message

--- test.c
+++ test.c
@@ -1,5 +1,39 @@
-#include<stdio.h>
-int main()
-{
-    print("This is my new project");
+#include <stdio.h>
+#include <sqlite3.h>
+
+// Callback function to process each row of the result
+static int callback(void *data, int argc, char **argv, char **azColName) {
+    for (int i = 0; i < argc; i++) {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+}
+
+int main() {
+    sqlite3 *db;
+    char *err_msg = 0;
+
+    // Open the database
+    int rc = sqlite3_open("test.db", &db);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return 1;
+    }
+
+    // Query the data
+    const char *sql = "SELECT * FROM Users;";
+    rc = sqlite3_exec(db, sql, callback, 0, &err_msg);
+
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        sqlite3_free(err_msg);
+    }
+
+    // Close the database
+    sqlite3_close(db);
+
+    return 0;
 }
