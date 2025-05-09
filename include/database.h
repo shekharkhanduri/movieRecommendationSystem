e809fbc47507756410c32b3b4c2f7671aa34dc2b
AgentLawliet <khandurishekhar246@gmail.com>
2025-05-09 22:43:42 +0530

    a mistake

--- include/database.h
+++
@@ -1,8 +0,0 @@
-#ifndef DATABASE_H
-#define DATABASE_H
-#include "sqlite3.h" 
-void search_by_column_text(const char *column, const char *value);
-void search_by_column_num(const char *column, const int value);
-sqlite3* open_database();
-
-#endif
